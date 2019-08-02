63181013b865e9366717e59a8f9170bbe15d6509
kikocorreoso <kachine@pm.me>
2019-08-02 20:18:39 +0200

    ported to pytest

--- skextremes/tests/test_datasets.py
+++ skextremes/tests/test_datasets.py
@@ -6,28 +6,47 @@ import unittest
 
 import numpy as np
 
-from skextremes.datasets import (dowjones, engine, euroex, exchange,
-                                        fremantle, glass, portpirie, rain,
-                                        venice, wavesurge, wind, wooster,
-                                        harris1996)
+from skextremes.datasets import (
+    dowjones,
+    engine,
+    euroex,
+    exchange,
+    fremantle,
+    glass,
+    portpirie,
+    rain,
+    venice,
+    wavesurge,
+    wind,
+    wooster,
+    harris1996,
+)
 
-datasets = (dowjones, engine, euroex, exchange, fremantle, glass, portpirie, 
-            rain, venice, wavesurge, wind, wooster, harris1996)
+datasets = (
+    dowjones,
+    engine,
+    euroex,
+    exchange,
+    fremantle,
+    glass,
+    portpirie,
+    rain,
+    venice,
+    wavesurge,
+    wind,
+    wooster,
+    harris1996,
+)
 
-class DatasetsTest(unittest.TestCase):
-    
-    def test_dataset_has_description(self):
-        for dataset in datasets:
-            self.assertIsInstance(dataset().description.__str__(), str)
-            
-    def test_dataset_asarray(self):
-        for dataset in datasets:
-            self.assertIsInstance(dataset().asarray(), np.ndarray)
-    
-    def test_dataset_has_fields(self):
-        for dataset in datasets:
-            self.assertTrue(hasattr(dowjones(), 'fields'))
 
+def test_dataset_has_description():
+    for dataset in datasets:
+        assert isinstance(dataset().description.__str__(), str)
 
-if __name__ == "__main__":
-    unittest.main(verbosity = 2)
+def test_dataset_asarray():
+    for dataset in datasets:
+        assert isinstance(dataset().asarray(), np.ndarray)
+
+def test_dataset_has_fields():
+    for dataset in datasets:
+        assert hasattr(dowjones(), "fields")
